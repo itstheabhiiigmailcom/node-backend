b1e5303638852940e6b57bdd70e6ba5353ff061b
itstheabhiiigmailcom <154773883+itstheabhiiigmailcom@users.noreply.github.com>
2024-11-19 13:23:09 +0530

    express is initialized and app is start listening

--- Readme.md
+++ Readme.md
@@ -1,8 +1,21 @@
 # backend with chai  aur code youtube playlist
 
-# npm i -D nodemon 
+## npm init
+# create public folder and temp folder inside it
+
+## npm i -D nodemon 
 # in script add dev: "nodemon src/index.js"
 
 # add type: "module" to use import instead of require
 
-# install prettier (npm i -D prettier) and create a file .prettierrc which contains configuration of prettier     (not important)
+## install prettier (npm i -D prettier) and create a file .prettierrc which contains configuration of prettier     (not important)
+
+# create all folders inside src (controllers, db, middlewares, models, routes, utils) and create index.js, constants.js, app.js(for express)
+
+## npm i mongoose express dotenv
+
+# establish database connection in db folder and import it into main index.js
+
+# initialize express in app.js and listen in index.js
+
+## install cookie-parser cors(cross origin resource sharing)
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+// import and run connectDB it will return promise
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongo connection Failed !!! ", err)
+})
+
+
+
+
+
+
+/*      approach to do evrything in index.js
+import express from 'express'
+const app = express()
+
+// iffe function with async
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (err) => {
+            console.log("My express is not eble to talk with db")
+            throw err
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on ${process.env.PORT}`)
+        })
+
+    }catch(error) {
+        console.log("ERROR: ", error)
+        throw error
+    }
+})() */
