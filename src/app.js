43f184f735eb6671094693ac6cbb6edbf0e389c1
itstheabhiiigmailcom <154773883+itstheabhiiigmailcom@users.noreply.github.com>
2024-11-20 16:28:39 +0530

    user registration controller and routes configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,21 @@ app.use(cors({
     credentials: true
 }))
 
-
 app.use(express.json({limit: "16kb"}))      // to accept json data
 app.use(express.urlencoded({extended: true, limit: "16kb"}))        
 app.use(express.static("public"))
 app.use(cookieParser())     //  a software library that extracts cookie data from HTTP requests and makes it available to the server-side code
+
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
 
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // creating a wrapper function to use all other functions
 // using promises
 const asyncHandler  = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
         .catch((err) => next(err))
     } 
